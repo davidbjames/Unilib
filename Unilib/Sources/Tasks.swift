ec978934a17833f6e8353cbb8913bbf569c0c3fd
David James <davidbjames1@gmail.com>
2016-10-05 16:37:14 +0100

    Nuked SingleTask for design reasons.

--- Unilib/Sources/Tasks.swift
+++ Unilib/Sources/Tasks.swift
@@ -18,14 +18,7 @@ public protocol Task {
     func create() -> Observable<Element>
 }
 
-/// A Task that normally returns an aggregate response (either as
-/// a single emission of an array or as multiple emmisions) that 
-/// will return the first of that aggregate. (Note that rx.single
-/// handles the latter but not the former.)
-public protocol SingleTask : Task {
-    associatedtype Single
-    func single() -> Observable<Single>
-}
+
 
 /// Options specific to handling a Task
 public struct TaskOptions: OptionSet {
@@ -44,24 +37,3 @@ public protocol Dependency {
     associatedtype Result
     func fulfill() -> Observable<Result>
 }
-
-
-
-
-// Rule not used, yet. Rules should have logical entry points, and not just adhoc wherever in the command you feel like.
-// A rule could be:
-//  - whether the command is fired in the first place
-//    i.e. it just goes straight to .error operator
-//  - whether the observable should fire .next or .error
-
-/*
-public protocol Rule {
-    associatedtype Condition
-    func evaluate(condition: Condition) -> Any?
-}
-
-public protocol Conditional {
-    associatedtype Rule
-    var rule:Rule { get set }
-}
-*/
