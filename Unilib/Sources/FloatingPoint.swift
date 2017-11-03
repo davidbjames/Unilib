e10a74a9f48a6371efe75ec8571f23030c68e01f
David James <davidbjames1@gmail.com>
2017-11-03 15:09:21 +0100

    Add FloatingPoint "rounded" and "isMultipleOf".

---
+++ Unilib/Sources/FloatingPoint.swift
@@ -0,0 +1,24 @@
+//
+//  FloatingPoint.swift
+//  Unilib
+//
+//  Created by David James on 02/11/2017.
+//  Copyright © 2017 David B James. All rights reserved.
+//
+
+import Foundation
+
+public extension FloatingPoint {
+    
+    /// Take a floating point number and round it to the
+    /// specified number of places.
+    func rounded(places:Int) -> Self {
+        let divisor = Self(Int(pow(10.0, Double(places))))
+        return (self * divisor).rounded() / divisor
+    }
+    
+    /// Is the current number a multiple of another.
+    func isMultipleOf(_ multiple:Self) -> Bool {
+        return self.truncatingRemainder(dividingBy: multiple) == 0
+    }
+}
