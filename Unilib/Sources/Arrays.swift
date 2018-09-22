76e65adc176087396fc55e1cb863bd7e9010eb0f
David James <davidbjames1@gmail.com>
2018-09-22 17:05:56 +0200

    Remove "shuffle" now that it's part of Swift.

--- Unilib/Sources/Arrays.swift
+++ Unilib/Sources/Arrays.swift
@@ -650,30 +650,3 @@ public extension Array where Element : Equatable {
     }
 }
 
-
-// Randomize array elements
-// From Nate Cook: http://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift#24029847
-
-public extension MutableCollection {
-    /// Shuffles the contents of this collection.
-    mutating func shuffle() {
-        let c = count
-        guard c > 1 else { return }
-        
-        for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
-            let d:Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
-            guard d != 0 else { continue }
-            let i = index(firstUnshuffled, offsetBy: d)
-            self.swapAt(firstUnshuffled, i)
-        }
-    }
-}
-
-public extension Sequence {
-    /// Returns an array with the contents of this sequence, shuffled.
-    func shuffled() -> [Iterator.Element] {
-        var result = Array(self)
-        result.shuffle()
-        return result
-    }
-}
